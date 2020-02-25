93c9d52abbb64dd12fe0229910fa8257b5e2df63
Andreas Gamborg <s163939@student.dtu.dk>
2020-02-25 15:46:00 +0100

    polar2carth done

--- polar2carth.m
+++ polar2carth.m
@@ -1,5 +1,14 @@
 function carth = polar2carth(polar)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+
+    r = polar(2,:);
+    theta = polar(1,:);
+    
+    x = cos(theta).*r;
+    y = sin(theta).*r;
+    
+    carth = [x; y];
+
 end
 
