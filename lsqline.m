89db3f4a15f27014acb2a63bea25fe26d6feca4b
Jeppe Heini Mikkelsen - s152939 <s152939@student.dtu.dk>
2020-02-25 17:14:31 +0100

    lsqline funktion
    lsqline funktion. Problemer med vinkler :(

--- lsqline.m
+++ lsqline.m
@@ -1,6 +1,31 @@
 function line = lsqline(points)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+
+    x = points(1,:);
+    y = points(2,:);
+
+    sigma_x = sum(x);
+    sigma_y = sum(y);
+    sigma_x2 = x*x';
+    sigma_y2 = y*y';
+    sigma_xy = x*y';
+
+    n = length(points);
+
+    mu_x = sigma_x/n;
+    mu_y = sigma_y/n;
+    
+    num = 2*sigma_x*sigma_y - 2*n*sigma_xy;
+    den = sigma_x^2 - sigma_y^2 - n*sigma_x2 + n*sigma_y2;
+
+    alpha = atan2(num,den)/2;
+        
+    r = abs(mu_x*cos(alpha) + mu_y*sin(alpha));
+    
+    % Vinkel problemer
+    
+    alpha = wrapToPi(alpha);
+    
+    line = [alpha,r];
 
 end
 
