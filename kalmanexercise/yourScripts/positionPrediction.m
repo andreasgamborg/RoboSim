106a865a034b4a38c0e090f9e9dff292353f6a4c
Jeppe Heini Mikkelsen - s152939 <s152939@student.dtu.dk>
2020-03-03 16:08:37 +0100

    Update positionPrediction.m

--- kalmanexercise/yourScripts/positionPrediction.m
+++ kalmanexercise/yourScripts/positionPrediction.m
@@ -14,10 +14,32 @@ global odoB kR kL
 
 %% pose update
 
-poseOut = [0;0;0];
+theta = poseIn(3);
+b = odoB;
+
+delS = (delSr + delSl)/2;
+delTheta = (delSr - delSl)/b;
+
+poseOut = poseIn + [delS*cos(theta + delTheta/2);
+                    delS*sin(theta + delTheta/2)
+                    delTheta];
+                
+poseOut(3) = wrapToPi(poseOut(3));
 
 %% Covariance update
-                
-covOut = zeros(3,3);
+
+dT = theta + delTheta/2;
+
+Fu = [cos(dT)/2 - delS/(2*b)*sin(dT), cos(dT)/2 + delS/(2*b)*sin(dT);
+      sin(dT)/2 + delS/(2*b)*cos(dT), sin(dT)/2 - delS/(2*b)*cos(dT);
+      1/b, -1/b];
+                                                                                             
+Qt = [kR*abs(delSr) 0; 0 kL*abs(delSl)];
+
+Fp = [1 0 -delS*sin(theta + delTheta/2);
+      0 1 delS*cos(theta + delTheta/2) ;
+      0 0                1             ];
+                                                         
+covOut = Fp*covIn*Fp' + Fu*Qt*Fu';
 
 end
