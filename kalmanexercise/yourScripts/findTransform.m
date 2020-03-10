8b1bd2f83d591d44e037f15439a0d9ccbb5a3f0e
PelleSchwartz <43444882+PelleSchwartz@users.noreply.github.com>
2020-03-10 15:15:05 +0100

    built findtransform and trans

--- kalmanexercise/yourScripts/findTransform.m
+++ kalmanexercise/yourScripts/findTransform.m
@@ -4,6 +4,11 @@ function transform = findTransform(odoPose, pose)
 % coordinates given a pose in the odometry coordinates (odoPose) and the
 % same point in the world coordinates (pose). The output (transform) is
 % simply the origo of the odometry coordinates in the world coordinates
-
-    transform = [0;0;0];
+    theta_T = odoPose(3) - pose(3);
+    
+    x_T = odopose(1) - (cos(theta_T) - sin(theta_T))*pose(1);
+    y_T = odopose(2) - (sin(theta_T) + cos(theta_T))*pose(2);
+    
+    
+    transform = [x_T;y_T;theta_T];
 end
--- kalmanexercise/yourScripts/trans.m
+++ kalmanexercise/yourScripts/trans.m
@@ -1,8 +1,15 @@
 function odoTargetPose = trans(transform,targetPose)
 % odoTargetPose = trans(transform,targetPose)
-% Transform a given point in world coordinates (targetPose) to odometry
+% Transform a given pose in world coordinates (targetPose) to odometry
 % coordinates, using the origo of the odometry coordinates in world
 % coordinates (transform).
 
-    odoTargetPose = [0;0;0];
+theta_o = targetPose(3) + transform(3);
+
+x_o = (cos(transform(3)) - sin(transform(3))) * targetPose(1) + transform(1);
+y_o = (sin(transform(3)) + cos(transform(3))) * targetPose(2) + transform(2);
+
+
+
+    odoTargetPose = [x_o;y_o;theta_o];
 end
