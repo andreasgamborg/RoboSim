9fcc49109742a7511960b6352807807b967f2d8f
Andreas Gamborg <s163939@student.dtu.dk>
2020-03-17 11:38:45 +0100

    fixed transforms

--- kalmanexercise/yourScripts/findTransform.m
+++ kalmanexercise/yourScripts/findTransform.m
@@ -1,14 +1,13 @@
-function transform = findTransform(odoPose, pose)
+function T = findTransform(odoPose, pose)
 % transform = FINDTRANSFORM(odoPose,pose)
 % Find the transformation from the world coordinates to the odometry
 % coordinates given a pose in the odometry coordinates (odoPose) and the
 % same point in the world coordinates (pose). The output (transform) is
 % simply the origo of the odometry coordinates in the world coordinates
-    theta_T = odoPose(3) - pose(3);
+    T = [0;0;0];
     
-    x_T = odoPose(1) - (cos(theta_T) - sin(theta_T))*pose(1);
-    y_T = odoPose(2) - (sin(theta_T) + cos(theta_T))*pose(2);
-    
-    
-    transform = [x_T;y_T;theta_T];
+    T(3) = odoPose(3) - pose(3);
+        
+    R = [cos(theta_T) -sin(theta_T); sin(theta_T) cos(theta_T)];
+    T(1:2) = odoPose(1:2)-R*pose(1:2);
 end
--- kalmanexercise/yourScripts/trans.m
+++ kalmanexercise/yourScripts/trans.m
@@ -3,13 +3,12 @@ function odoTargetPose = trans(transform,targetPose)
 % Transform a given pose in world coordinates (targetPose) to odometry
 % coordinates, using the origo of the odometry coordinates in world
 % coordinates (transform).
-
-theta_o = targetPose(3) + transform(3);
-
-x_o = (cos(transform(3)) - sin(transform(3))) * targetPose(1) + transform(1);
-y_o = (sin(transform(3)) + cos(transform(3))) * targetPose(2) + transform(2);
-
-
-
-    odoTargetPose = [x_o;y_o;theta_o];
+   
+    odoTargetPose = [0;0;0];
+    theta_T =  transform(3);
+    
+    odoTargetPose(3) = targetPose(3) + theta_T;
+    
+    R = [cos(theta_T) -sin(theta_T); sin(theta_T) cos(theta_T)];
+    odoTargetPose(1:2) = R*pose(1:2)+transform(1:2) ;
 end
