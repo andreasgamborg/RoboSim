73930d678d20b1955611115bc03500bd5a04336a
Andreas Gamborg <s163939@student.dtu.dk>
2020-03-17 15:30:11 +0100

    mainLoop run square

--- kalmanexercise/yourScripts/mainLoop.m
+++ kalmanexercise/yourScripts/mainLoop.m
@@ -18,10 +18,17 @@ matchPose = pose; %Recording the pose during mathing for plotting purposes
 [pose, poseCov] = measurementUpdate(pose,poseCov, matchResult);
 
 % Move a bit
-
 % Find the next waypoint in world coordinates and put it in targetPose
-targetPose = [0; 0; 0];
+%Corners = [[0.5 0.5 pi]' [0 0 0]' [-0.5 0.5 pi/2]' [0 0 0]' [-0.5 -0.5 0]' [0 0 0]' [0.5 -0.5 pi/2]' [0 0 0]' [0.5 0.5 pi]' [0 0 0]'];
+Roundtrip = [[0.5 0.5 pi]' [-0.5 0.5 pi/2]' [-0.5 -0.5 0]' [0.5 -0.5 pi/2]'];
 
+targetPose = Roundtrip(:,mod(currentWaypoint,4)+1);
+
+dist = sqrt(sum((targetPose(1:2)-pose(1:2)).^2));
+
+if dist < 0.1
+    currentWaypoint = currentWaypoint+1;
+end
 
 %Find the transformation from the estimated world coordinates to the 
 %odometry coordinates
